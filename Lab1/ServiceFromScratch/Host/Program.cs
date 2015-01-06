fd27e733fcd1838f8951fc0c582cfa785c98287e
Elgardo Echevarria <elgardoe@gmail.com>
2015-01-05 23:00:16 -0500

    Learning WCF Lab, Lab 1

---
+++ Lab1/ServiceFromScratch/Host/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+
+namespace ServiceFromScratch
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         using(ServiceHost host = new ServiceHost(typeof(HelloIndigoService), 
+            new Uri("http://localhost:8000/HelloIndigo")))
+         {
+            host.AddServiceEndpoint(typeof(IHelloIndigoService),
+               new BasicHttpBinding(), "HelloIndigoService");
+            host.Open();
+            Console.WriteLine("Press <ENTER> to terminate the service hook");
+            Console.ReadLine();
+         }
+      }
+   }
+}
